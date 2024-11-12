9d2ae1be1c10ebd1c1dc34f57114a2b14f431028
Annenkov Maksim <bakeeva@yandex.ru>
2024-11-12 14:08:43 +0300

    save 1

---
+++ src/main/java/ru/netology/MaxonHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxonHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxonHibernateApplication.class, args);
+    }
+
+}
